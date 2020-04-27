6c0cdfd456d213894b8ca6d9fe9c49f30dccdb07
Malik <ymaliksaqib@gmail.com>
2020-04-27 13:56:45 +0500

    Alerts

--- Components/FavoriteComponent.js
+++ Components/FavoriteComponent.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, FlatList, Text } from 'react-native';
+import {View, FlatList, Text, Alert } from 'react-native';
 import {ListItem} from 'react-native-elements';
 import {DISHES} from '../Shared/dishes';
 import { createStackNavigator } from 'react-navigation';
@@ -32,7 +32,16 @@ class Favorite extends Component {
                 {
                     text: 'Delete',
                     type: 'delete',
-                    onPress: () => this.props.deleteFavorite(item.id)
+                    onPress: () => {
+                        Alert.alert(
+                            'Delete Favorite?',
+                            'Are you sure you wish to delete the favorite dish' + item.name + '?',
+                            [
+                                {text: 'Cancel', onPress: () => console.log(item.name + 'Not Deleted!'), style: 'cancel'},
+                                {text: 'OK', onPress : () => this.props.deleteFavorite(item.id)}
+                            ]
+                        );
+                    }
                 }
             ];
             return(
